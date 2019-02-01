3f0374f2f2de38aa1da6c53ea30d6ff729cc3b68
johnr14 <jonathanrioux@gmail.com>
2019-01-31 19:09:05 -0500

    Fixes

--- client-scripts/04-install-net.ps1
+++ client-scripts/04-install-net.ps1
@@ -1,4 +1,5 @@
 set ERRORLOG=c:\CHOCOERROR.LOG
 
-choco install dropbox filezilla firefox putty winscp --acceptlicense -y --package-parameters "--includeRecommended" -c D:\To-Install\Chocolatey\DL
+choco install dropbox filezilla putty winscp --acceptlicense -y --package-parameters "--includeRecommended" -c D:\To-Install\Chocolatey\DL
+choco install firefox --acceptlicense -y -c D:\To-Install\Chocolatey\DL
 
--- client-scripts/06-install-dev.ps1
+++ client-scripts/06-install-dev.ps1
@@ -1,6 +1,6 @@
 set ERRORLOG=c:\CHOCOERROR.LOG
 
-choco install arduino atom eclipse git.install jdk8 jre8 javaruntime nodejs.install python3 VisualStudio2017Community visualstudio2017-workload-webbuildtools windows-sdk-8.1 --acceptlicense -y --package-parameters "--includeRecommended" -c D:\To-Install\Chocolatey\DL
+choco install arduino atom eclipse git.install nodejs.install python3 VisualStudio2017Community visualstudio2017-workload-webbuildtools windows-sdk-8.1 --acceptlicense -y --package-parameters "--includeRecommended" -c D:\To-Install\Chocolatey\DL
 choco install jdk8 jre8 javaruntime --acceptlicense -y -c D:\To-Install\Chocolatey\DL
 
 
